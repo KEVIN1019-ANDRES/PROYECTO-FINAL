08420719f497b246c9e1bf9444f8f71df57c8fe2
elpoyon <arizafontechanicolas@gmail.com>
2024-09-17 09:13:38 -0500

    filtro de busqueda agregado
    ya puse el filtro de busqueda

--- app/routes/Vista_routes.py
+++ app/routes/Vista_routes.py
@@ -1,11 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app.admin.models.vehiculo import Vehiculo
-from flask import Blueprint, render_template, request, redirect, url_for, jsonify
-
 
 bp = Blueprint('moto_details', __name__)
 
 @bp.route('/')
 def index():
-    data = Vehiculo.query.all()
-    return render_template('vista/index.html', data=data)
+    # Obtener el número de página, por defecto será la primera
+    page = request.args.get('page', 1, type=int)
+
+    # Obtener el término de búsqueda del usuario, si existe
+    search_query = request.args.get('search', '')
+    page = request.args.get('page', 1,  type=int)
+
+    # Filtrar vehículos basados en la consulta de búsqueda
+    print(f"Entra el index -----------{search_query}----   ---------------serach queresafadf")
+    if search_query:
+        vehiculos = Vehiculo.query.filter(
+            Vehiculo.marca.ilike(f'%{search_query}%') |
+            Vehiculo.modelo.ilike(f'%{search_query}%') |
+            Vehiculo.color.ilike(f'%{search_query}%')
+        ).paginate(page=page, per_page=10)
+    else:
+        vehiculos = Vehiculo.query.paginate(page=page, per_page=10)
+
+
+    # Renderizar la vista principal con los resultados paginados
+    return render_template('vista/index.html', vehiculos=vehiculos)
