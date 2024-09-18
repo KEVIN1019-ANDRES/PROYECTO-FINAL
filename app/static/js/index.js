bc7fb2dbce6a8ce51a3ca20499ce043848d8f96d
KEVIN1019-ANDRES <138932402+KEVIN1019-ANDRES@users.noreply.github.com>
2024-09-18 11:32:56 -0500

    cambio 2

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,35 +1,7 @@
+var mainListDiv = document.getElementById("mainListDiv");
+var mediaButton = document.getElementById('mediaButton');
 
-document.addEventListener('DOMContentLoaded', function () {
-    const track = document.querySelector('.theme_brands');
-    let isMouseDown = false;
-    let startX;
-    let scrollLeft;
-
-    track.addEventListener('mousedown', (e) => {
-        isMouseDown = true;
-        startX = e.pageX - track.offsetLeft;
-        scrollLeft = track.scrollLeft;
-    });
-
-    track.addEventListener('mouseleave', () => {
-        isMouseDown = false;
-    });
-
-    track.addEventListener('mouseup', () => {
-        isMouseDown = false;
-    });
-
-    track.addEventListener('mousemove', (e) => {
-        if (!isMouseDown) return;
-        e.preventDefault();
-        const x = e.pageX - track.offsetLeft;
-        const walk = (x - startX) * 2; // Ajusta la velocidad del desplazamiento
-        track.scrollLeft = scrollLeft - walk;
-
-        if (track.scrollLeft === 0) {
-            track.scrollLeft = track.scrollWidth / 2;
-        } else if (track.scrollLeft >= track.scrollWidth / 2) {
-            track.scrollLeft = track.scrollWidth / 4;
-        }
-    });
-});
+mediaButton.onclick = function () {
+    mainListDiv.classList.toggle('show_list');
+    mediaButton.classList.toggle('active');
+}
