039bbace0d62ee22e2a2860241089e590fcf394a
Samuel9701 <bejaranosamuel28@gmail.com>
2024-09-18 08:01:36 -0500

    olaaaa

--- app/models/detalle.py
+++ app/models/detalle.py
@@ -6,7 +6,7 @@ class Detalle(db.Model):
     cantidad = db.Column(db.String(255), nullable=False)
     precio = db.Column(db.String(255), nullable=False)
     iva = db.Column(db.String(255), nullable=False)
-    vehiculo_id = db.Column(db.Integer, db.ForeignKey('vehiculo.id'))
+    vehiculo_id = db.Column(db.Integer, db.ForeignKey('vehiculos.id'))  
     producto_id = db.Column(db.Integer, db.ForeignKey('producto.id'))
 
     producto = db.relationship("Producto", back_populates="detalles")
