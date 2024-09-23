c3e469337f5f9ea3334f4283809d2095a58800da
KEVIN1019-ANDRES <138932402+KEVIN1019-ANDRES@users.noreply.github.com>
2024-09-23 10:49:55 -0500

    Cambios en vista
    Cambios en el carrucel

--- app/static/js/index.js
+++
@@ -1,7 +0,0 @@
-var mainListDiv = document.getElementById("mainListDiv");
-var mediaButton = document.getElementById('mediaButton');
-
-mediaButton.onclick = function () {
-    mainListDiv.classList.toggle('show_list');
-    mediaButton.classList.toggle('active');
-}
