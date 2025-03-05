8ccd1537962a88048426c89facade2bb4ab3551c
elpoyon <arizafontechanicolas@gmail.com>
2025-03-05 11:37:04 -0500

    Update usuario.py
    mnjh

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,8 +1,9 @@
+import json
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
-
+from typing import Optional, Dict, Any
 
 class Usuario(db.Model, UserMixin):
     __tablename__ = 'usuario'
